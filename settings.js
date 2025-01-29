675eb385112b92d7fff720122751e141da5cf7b8
kriito212 <63334721+kriito212@users.noreply.github.com>
2025-01-29 01:31:58 -0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'NamaOwnerLu', // Ganti dengan nama owner lu
+  botToken: '7944249117:AAEmxDYpMFvVuQ6AiTefYAIqyGE5LrhFiNE', // Ganti dengan token bot Telegram lu
+  owner: '6361969853', //OWNER user id
+};
+
+module.exports = settings;
